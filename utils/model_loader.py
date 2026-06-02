90951b5e2f116d73e586cbdf39216e129795bf8f
anubhabde1989 <anubhabde1989@gmail.com>
2026-06-03 01:33:02 +0530

    save changes

---
+++ utils/model_loader.py
@@ -0,0 +1,89 @@
+import os
+import sys
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from utils.config_loader import load_config
+from langchain_groq import ChatGroq
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+
+log = CustomLogger().get_logger(__name__)
+
+class ModelLoader:
+    def __init__(self):
+        load_dotenv()
+        self._validate_env()
+        self.config = load_config()
+        log.info("Configuration loaded successfully", config_key=list(self.config.keys()))
+
+    def _validate_env(self):
+        """
+        Validate necessary environment variables
+        """
+        required_vars = ['GEMINI_API_KEY','GROQ_API_KEY']
+        self.api_keys = {key:os.getenv(key) for key in required_vars}
+        missing = [k for k,v in self.api_keys.items() if not v]
+        if missing:
+            log.error("Missing environment variables:", missing_vars=missing)
+            raise DocumentPortalException("Missing environment variables",sys)
+
+    def load_embedding(self):
+        """Load and return the embedding model"""
+        try:
+            log.info("Loading embedding model...")
+            model_name = self.config["embedding_model"]["model_name"]
+            return GoogleGenerativeAIEmbeddings(model = model_name)
+        except Exception as e:
+            log.error("Error loading embedding model", error = str(e))
+            raise DocumentPortalException("Failed to load embedding model", sys)
+        
+    def load_llm(self):
+        """
+        Load and return the LLM Model
+        """
+        """Load LLM dynamically based on provider in configuration"""
+        llm_block = self.config["llm"]
+        # Default Provider or choose from environment variable
+        provider_key = os.getenv("LLM_PROVIDER","groq") #Default groq
+
+        if provider_key not in llm_block:
+            log.error("LLM provider not found in config", provider_key=provider_key)
+            raise ValueError(f"Provider '{provider_key}' not found in config")
+        
+        llm_config = llm_block[provider_key]
+        provider = llm_config.get("provider")
+        model_name = llm_config.get("model_name")
+        temperature = llm_config.get("temperature", 0.2)
+        max_tokens = llm_config.get("max_tokens", 2048)
+
+        log.info("Loading LLM", provider=provider, model_name=model_name, temperature=temperature, max_tokens=max_tokens)
+
+        if provider == "google":
+            llm = ChatGoogleGenerativeAI(model=model_name, temperature = temperature, max_tokens = max_tokens)
+            return llm
+        elif provider == "groq":
+            llm = ChatGroq(model=model_name, api_key=self.api_keys["GROQ_API_KEY"], temperature = temperature, max_tokens = max_tokens) 
+            return llm 
+        elif provider == "openai":
+            return ChatOpenAI(model=model_name, api_key=self.api_keys["OPENAI_API_KEY"], temperature = temperature, max_tokens = max_tokens)
+        else:
+            log.error("Unsupported LLM provider", provider=provider)
+            raise ValueError(f"Unsupported LLM provider: {provider}")
+        
+if __name__ == "__main__":
+    loader = ModelLoader()
+
+    #Test Embedding Model Loading 
+    embeddings = loader.load_embedding()
+    print(f"Embedding model loaded successfully: {embeddings}")
+    result = embeddings.embed_query("What is the capital of France?")
+    print(f"Embedding result: {result}")
+
+    #Test LLM loading based on YAML configuration
+    llm = loader.load_llm()
+    print(f"LLM loaded successfully: {llm}")
+
+    #Test ModelLoader
+    result = llm.invoke("What is the capital of France?")
+    print(f"LLM Response: {result.content}")
