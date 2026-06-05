82c676cb035c884e21b6e5443d5b9ea282535c82
anubhabde1989 <anubhabde1989@gmail.com>
2026-06-05 17:47:33 +0530

    commit

--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -1,10 +1,12 @@
 import os 
+import sys
 from utils.model_loader import ModelLoader
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from model.models import * 
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.output_parsers import OutputFixingParser
+from datetime import datetime
 
 class DocumentAnalyzer:
     """Analyzes documents using a pre-trained model.
@@ -13,11 +15,7 @@ class DocumentAnalyzer:
     def __init__(self):
         pass
 
-    def analyze_metadata(self, document_path):
-        try:
-            model = self.model_loader.load_model()
-            # Assuming the model has a method analyze to process the documents
-            analysis_result = model.analyze(document_path)
-            return self.fixing_parser.parse(analysis_result)
-        except Exception as e:
-            raise DocumentPortalException(f"Error analyzing document",e) from e
+    def analyze_document(self):
+        pass
+
+    
--- src/document_analyzer/data_ingestion.py
+++ src/document_analyzer/data_ingestion.py
@@ -1,4 +1,5 @@
 import os 
+import sys
 import fitz
 import uuid
 from datetime import datetime 
@@ -10,13 +11,32 @@ class DocumentHandler:
     Handles PDF saving and reading operations.
     Automatically logs all actions and creates session-based organization.
     """
-    def __init__(self):
-        pass
+    def __init__(self, data_dir=None, session_id=None):
+        try:
+            self.log = CustomLogger().get_logger(__name__)
+            self.data_dir = data_dir or os.getenv("DATA_STORAGE_PATH",os.path.join(os.getcwd(),"data","document_analysis"))
+            self.session_id = session_id or f"session_{datetime.utcnow().strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex[:8]}"
+            self.session_path = os.path.join(self.data_dir, self.session_id)
+            os.makedirs(self.session_path, exist_ok=True)
+            self.log.info("PDFHandler initialized", session_id=self.session_id, session_path=self.session_path)
+        except Exception as e:
+            raise DocumentPortalException("Error initializing DocumentHandler", sys) from e
 
-    def save_pdf(self):
-        pass 
-
+    def save_pdf(self, pdf_data):
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error saving PDF: {e}")
+            raise DocumentPortalException("Error saving PDF", sys) from e 
+                
     def read_pdf(self):
-        pass 
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error reading PDF: {e}")
+            raise DocumentPortalException("Error reading PDF", sys) from e
 
-    
+if __name__ == "__main__":
+    handler = DocumentHandler()
+    print(f"Session ID: {handler.session_id}")
+    print(f"Session path: {handler.session_path}")
